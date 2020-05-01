650c4aee33b9015911fd54957b07483b437df548
Yahor <yegor.yanush@gmail.com>
2020-05-01 13:52:22 +0300

    Updates

---
+++ precache-manifest.be1238ba95dc7044b7762b53509c5486.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bc536e9b272336505fe5c68ca2bdd8a",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "1fa56bffa62e20e9b59c",
+    "url": "/Portfolio/static/css/main.225e9084.chunk.css"
+  },
+  {
+    "revision": "dca7cbdd602e0bdb26bd",
+    "url": "/Portfolio/static/js/2.7914e8cc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portfolio/static/js/2.7914e8cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fa56bffa62e20e9b59c",
+    "url": "/Portfolio/static/js/main.00c8ce47.chunk.js"
+  },
+  {
+    "revision": "4a50002865ce6bd649aa",
+    "url": "/Portfolio/static/js/runtime-main.d26897fe.js"
+  }
+]);
